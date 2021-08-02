a81cb8455c061247dcc1780bfedba5b91ddf1677
devncore-kevin <68521148+devncore-kevin@users.noreply.github.com>
2021-08-02 09:09:22 +0900

    Update Startup.cs

--- Dev-Community/Startup.cs
+++ Dev-Community/Startup.cs
@@ -56,5 +56,81 @@ namespace Dev_Community
                 endpoints.MapFallbackToPage("/_Host");
             });
         }
+        
+                //public void ConfigureServices(IServiceCollection services)
+        //{
+        //    var serverVersion = new MariaDbServerVersion(new Version(10, 0, 16));
+
+        //    services.AddDbContext<EtoosContext>(options =>
+        //          options.UseLazyLoadingProxies()
+        //          .UseMySql(Configuration.GetConnectionString("etoos"), serverVersion)); 
+
+        //    services.AddDefaultIdentity<User>(options => options.SignIn.RequireConfirmedAccount = true)
+        //            .AddRoles<NewUserRole>()
+        //            .AddEntityFrameworkStores<EtoosContext>();  
+
+        //    services.Configure<IdentityOptions>(options =>
+        //    {
+        //        //previous code removed for clarity reasons
+        //        options.Lockout.AllowedForNewUsers = true;
+        //        options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(550);
+        //        options.Lockout.MaxFailedAccessAttempts = 5;
+        //    });
+
+
+        //    services.AddCors(options =>
+        //    {              
+        //        options.AddDefaultPolicy(builder =>
+        //        {
+        //            builder
+        //                .WithOrigins(new string[]
+        //                {
+        //                    "https://34.64.71.30",
+        //                    "https://centervr.storage.googleapis.com"
+        //                });
+        //        });
+        //    });
+
+        //    //services.AddDefaultIdentity<UserInfo>(options => options.SignIn.RequireConfirmedAccount = true)
+        //    //    .AddEntityFrameworkStores<EtoosContext>();
+
+        //    services.AddAuthentication()
+        //            .AddCookie(CookieAuthenticationDefaults.AuthenticationScheme);
+
+        //    services.AddScoped<AuthenticationStateProvider, RevalidatingIdentityAuthenticationStateProvider<User>>();
+
+        //    services.AddSyncfusionBlazor();
+        //    services.AddRazorPages();
+        //    services.AddServerSideBlazor();
+
+        //    services.AddTransient<IFileUploadService, GoogleUploadService>();
+        //    services.AddSingleton<DragAndDropService>();
+
+        //    //DBService
+        //    AddTransient(services);
+        //    //로그
+        //    SerilogConfig(services);            
+        //}
+
+        //public void AddTransient(IServiceCollection services)
+        //{
+        //    services.AddTransient<ISvcCdService, SvcCdService>();
+        //    services.AddTransient<ILectureService, LectureService>();
+        //    services.AddTransient<IExamService, ExamService>();
+        //    services.AddTransient<IStadiumService, StadiumService>();
+        //    services.AddTransient<IExamResultService, ExamResultService>();
+        //    services.AddTransient<IRoomService, RoomService>();
+        //    services.AddTransient<IPlaceService, PlaceService>();
+        //    services.AddTransient<IRoomTemplateService, RoomTemplateService>();
+        //    services.AddTransient<IQtalkService, QtalkService>();
+        //    services.AddTransient<IPlaceRoomService, PlaceRoomService>();
+        //    services.AddTransient<IUserFileService, UserFileDBService>();
+        //    services.AddTransient<ICategoryService, CategoryService>();
+        //    services.AddTransient<IRoomReservationService, RoomReservationService>();
+        //    services.AddTransient<IUserInfoService, UserInfoService>();
+        //    services.AddTransient<IRoomDetailService, RoomDetailService>();
+        //    services.AddTransient<IRoomQuestionService, RoomQuestionService>();
+        //    services.AddTransient<IQtalkNoticeService, QtalkNoticeService>();
+        //}
     }
 }
